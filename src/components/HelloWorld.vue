39c13346fcadb7dc93b93d37c650f341f0abcd28
nyasha <nchasakara@gmail.com>
2018-09-16 22:34:12 +0200

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,113 +1,63 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Join Now</h2>
-    <ul>
-      <li>
-        <a
-          href="https://facebook.com/techhubharare"
-          target="_blank"
-        >
-          Facebook
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/hararehub"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://discordapp.com/channels/427674720574636042/427674720574636044"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://docs.google.com/forms/d/1faE1qlgFgvN3dLPqmsxlDh5qWMT_7LY0NCLFmsjBFVM"
-          target="_blank"
-        >
-          Sign Up
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="https://docs.google.com/forms/d/1faE1qlgFgvN3dLPqmsxlDh5qWMT_7LY0NCLFmsjBFVM"
-          target="_blank"
-        >
-          Sign Up
-        </a>
-      </li>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-  </div>
+  <section>
+    <div class="form-group">
+      <label>Select a date</label>
+      <div class="input-group">
+        <flat-pickr
+                v-model="date"
+                :config="config"                                                          
+                class="form-control" 
+                placeholder="Select date"               
+                name="date">
+        </flat-pickr>
+        <div class="input-group-btn">
+          <button class="btn btn-default" type="button" title="Toggle" data-toggle>
+            <i class="fa fa-calendar">
+              <span aria-hidden="true" class="sr-only">Toggle</span>
+            </i>
+          </button>
+          <button class="btn btn-default" type="button" title="Clear" data-clear>
+            <i class="fa fa-times">
+              <span aria-hidden="true" class="sr-only">Clear</span>
+            </i>               
+          </button>
+        </div>
+      </div>
+    </div>
+    <pre>Selected date is - {{date}}</pre>
+  </section>
 </template>
 
 <script>
+// bootstrap is just for this example
+import "bootstrap/dist/css/bootstrap.css";
+// import this component
+import flatPickr from "vue-flatpickr-component";
+import "flatpickr/dist/flatpickr.css";
+// theme is optional
+// try more themes at - https://chmln.github.io/flatpickr/themes/
+import "flatpickr/dist/themes/material_blue.css";
+// l10n is optional
+import { Hindi } from "flatpickr/dist/l10n/hi";
+
 export default {
-  name: "HelloWorld",
+  name: "yourComponent",
   data() {
     return {
-      msg: "Welcome to our Entrepreneurship club"
+      // Initial value
+      date: new Date(),
+      // Get more form https://chmln.github.io/flatpickr/options/
+      config: {
+        wrap: true, // set wrap to true only when using 'input-group'
+        altFormat: "M	j, Y",
+        altInput: true,
+        dateFormat: "Y-m-d",
+        locale: Hindi // locale for this instance only
+      }
     };
+  },
+  components: {
+    flatPickr
   }
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1,
-h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
